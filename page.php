ef7ea1a27dc434ef9c6137b9aa7fbec1c64a3f5a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-06-27 09:30:52 -0500

    [+] WP|Page: Integra 'page.php'

--- page.php
+++ page.php
@@ -15,26 +15,76 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<main id="main" class="site-main">
+	<div class="page-us">	
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<section class="business">
+				
+			<?php 
+				while ( have_posts() ) :
+					the_post();
+			
+					get_template_part( 'template-parts/content-page', 'nosotros' );
+			
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-			get_template_part( 'template-parts/content', 'page' );
+				endwhile; // End of the loop.
+			?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		</section><!-- .business -->
 
-		endwhile; // End of the loop.
-		?>
+		<section class="our-services">
+    
+            <div class="container">
+                <hgroup>
+                    <h3>Trabajamos con los mejores</h3>
+                    <h2>Nuestros proveedores destacados</h2>
+                </hgroup>
+                <div id="carousel-services" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner card-deck row w-100 mx-auto" role="listbox">
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active">
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/1.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img1">    
+                        </div>
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/2.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img2">
+                        </div>
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/3.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img3">
+                        </div>
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/4.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img4">
+                        </div>
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/5.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img5">
+                        </div>
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">    
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/6.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img6">
+                        </div>
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/7.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img7">
+                        </div>
+                        <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                            <img src="<?php echo get_template_directory_uri() .'/dist/assets/images/services/8.jpg'; ?>" class="card--image card-img-top img-fluid mx-auto d-block" alt="img8">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carousel-services" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel-services" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Siguiente</span>
+                    </a>
+                </div>
+            </div>
+        </section><!-- .our-services -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div><!-- .page-us -->
+</main><!-- #main -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,44 +9,61 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<div class="business--header">
+	<div class="container">
+		<div class="page-header">
 
-	<?php abc_servitodo_post_thumbnail(); ?>
+			<hgroup class="page--title">
+				<?php 
+					$subtitle = abc_servitodo_cmb2_get_field_value( get_the_ID(), 'page_subtitle' );
+					if( $subtitle[ 'exists' ] ) :
+				?>
 
-	<div class="entry-content">
-		<?php
-		the_content();
+					<h3><?php echo $subtitle[ 'value' ]; ?></h3>
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'abc-servitodo' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+				<?php endif; ?>
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</hgroup>
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'abc-servitodo' ),
-						array(
-							'span' => array(
-								'class' => array(),
+			<?php abc_servitodo_post_thumbnail(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+				<?php if ( get_edit_post_link() ) : ?>
+					<footer class="entry-footer">
+						<?php
+						edit_post_link(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Edit <span class="screen-reader-text">%s</span>', 'abc-servitodo' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								get_the_title()
 							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+							'<span class="edit-link">',
+							'</span>'
+						);
+						?>
+					</footer><!-- .entry-footer -->
+				<?php endif; ?>
+				
+			</article><!-- #post-<?php the_ID(); ?> -->
+
+			<div class="entry-content">
+				<?php
+				the_content();
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'abc-servitodo' ),
+					'after'  => '</div>',
+				) );
+				?>
+			</div><!-- .entry-content -->
+		</div>
+	</div>
+</div>
